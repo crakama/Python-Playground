8fdd7a9859164376b5009f2fcd6a2ce9ba201e90
crakama <crakama89@gmail.com>
2016-05-16 23:52:55 +0300

    data types

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+"""
+Complete the code in the editor below. The variables i, d, and s are already declared and initialized for you. You must declare 3 variables: one of type int, one of type double, and one of type String. Then you must read 3 lines of input from stdin and initialize your 3 variables. Finally, you must use the + operator to perform the following operations:
+
+Print the sum of i plus your int variable on a new line.
+Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+Concatenate s with the string you read as input and print the result on a new line.
+
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+
+"""
+i = 4
+d = 4.0
+s = 'Rank'
+
--- puzzles.py
+++ puzzles.py
@@ -1,6 +1,6 @@
-""" 
+"""
     The code does not execute properly. Try to figure out why. 
     def multiply(a, b):
         a * b
-        AssertException(message) cw-2.AssertException: None should equal 1
+      AssertException(message) cw-2.AssertException: None should equal 1
 """
