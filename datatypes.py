9a4ff39f0c31ad824d8263ef75bc8f1afb01be15
crakama <crakama89@gmail.com>
2016-05-16 23:54:26 +0300

    edit datatypes

--- datatypes.py
+++ datatypes.py
@@ -13,4 +13,21 @@ Concatenate  with the string you read as input and print the result on a new lin
 i = 4
 d = 4.0
 s = 'Rank'
+# Declare second integer, double, and String variables.
+i2 = int(raw_input())
+d2 = float(raw_input())
+s2 = str(raw_input())
 
+# Read and save an integer, double, and String to your variables.
+
+
+# Print the sum of both integer variables on a new line.
+
+print i + i2
+
+
+# Print the sum of the double variables on a new line.
+print d + d2
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + s2
