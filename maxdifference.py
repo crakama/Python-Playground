557249119bf0c05a0832db2582f5919c2eb1874b
crakama <crakama89@gmail.com>
2016-05-27 08:40:05 +0300

    abstract classes

--- maxdifference.py
+++ maxdifference.py
@@ -1,11 +1,15 @@
 """
 Complete the Difference class by writing the following:
 
-A class constructor that takes an array of integers as a parameter and saves it to the  instance variable.
-A computeDifference method that finds the maximum absolute difference between any  numbers in  and stores it in the  instance variable
+A class constructor that takes an array of integers as a parameter
+and saves it to the  instance variable.
+A computeDifference method that finds the maximum absolute difference
+
+between any  numbers in  and stores it in the  instance variable
 
 
 """
+
 
 class Difference:
     def __init__(self, a):
