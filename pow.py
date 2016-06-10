21860cb4d71e71d95644f1d7638b89a7cf7a8f7e
crakama <crakama89@gmail.com>
2016-06-10 21:04:25 +0300

    pow inbuilt method

---
+++ pow.py
@@ -0,0 +1,18 @@
+"""
+    You are given three integers: , , and , respectively. Print two lines. 
+    The first line should print the result of pow(a,b). 
+    The second line should print the result of pow(a,b,m).
+
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+
+a = int(raw_input())
+b = int(raw_input())
+m = int(raw_input())
+
+twopower = pow(a, b)
+threepower = pow(a, b, m)
+print twopower
+print threepower
