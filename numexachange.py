8b9a3ab54274bf1bf5bf3afdc9d5909c7414235f
crakama <crakama89@gmail.com>
2016-06-18 23:58:52 +0300

    exchange numbers

---
+++ numexachange.py
@@ -0,0 +1,8 @@
+""" 
+   You are given two integers. Store them into two variables and 
+   then exchange them. Rather than using any fancy logic, 
+   make sure to use a tuple to do the task. Print the two numbers.
+"""
+a = input(), input() 
+print a[1] 
+print a[0]
